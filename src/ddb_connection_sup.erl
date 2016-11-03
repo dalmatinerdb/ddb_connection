60d133210535dd7115796d2d1ff43fdbe06cea39
Heinz N. Gies <heinz@licenser.net>
2016-11-03 19:58:46 +0100

    Some cleanup on the ddb connection setup

--- src/ddb_connection_sup.erl
+++ src/ddb_connection_sup.erl
@@ -8,10 +8,12 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, endpoint/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
+
+-ignore_xref([endpoint/0]).
 
 -define(POOL, backend_connection).
 
@@ -28,16 +30,7 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-    {Host, Port} = case application:get_env(ddb_connection, backend) of
-                       {ok, {Host, Port}} ->
-                           {Host, Port};
-                       _ ->
-                           {ok, Host} = application:get_env(ddb_connection,
-                                                            backend_host),
-                           {ok, Port} = application:get_env(ddb_connection,
-                                                            backend_port),
-                           {Host, Port}
-                   end,
+    {Host, Port} = endpoint(),
     {ok, PoolSize} = application:get_env(ddb_connection, pool_size),
     {ok, PoolMax} = application:get_env(ddb_connection, pool_max),
     SizeArgs = [
@@ -49,3 +42,15 @@ init([]) ->
     WorkerArgs = [Host, Port],
     {ok, {{one_for_one, 5, 10},
           [poolboy:child_spec(?POOL, PoolArgs, WorkerArgs)]}}.
+
+endpoint() ->
+    case application:get_env(ddb_connection, backend) of
+        {ok, {Host, Port}} ->
+            {Host, Port};
+        _ ->
+            {ok, Host} = application:get_env(ddb_connection,
+                                             backend_host),
+            {ok, Port} = application:get_env(ddb_connection,
+                                             backend_port),
+            {Host, Port}
+    end.
